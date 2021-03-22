d0c192557f7e1f3991c02afe21121265583c2dd2
Minhyeong Jang <public.doriri@gmail.com>
2021-03-22 20:46:35 +0900

    feat: 불필요 코드 제거 및 유틸 코드 추가

--- src/modules/shared/components/Select/Select.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
-
-export const Select: FC = () => {
-  return <StyledContainer></StyledContainer>;
-};
-const StyledContainer = styled.div``;
