0e0788c1d326b9098cb102e8a79ea78a0967d74b
Minhyeong Jang <public.doriri@gmail.com>
2024-04-04 17:51:49 +0900

    Updates

---
+++ assets/SamplePage-AJu1ibOG.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dig9Vazs.js";import{D as e}from"./DutchPageTemplate-_upeBCSY.js";import"./NavibarContainer-rJyZWsBf.js";const o=()=>t.jsx(e,{children:"test"});export{o as default};
--- assets/SamplePage-VxwiHYxu.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-I2TEKTUE.js";import{D as e}from"./DutchPageTemplate-pT28SX8i.js";import"./NavibarContainer-d2Ol_j1s.js";const o=()=>t.jsx(e,{children:"test"});export{o as default};
